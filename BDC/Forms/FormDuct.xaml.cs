3a1f44e648d79f69fb68216a960ae208a6289e01
javadansari <javad.ansari@gmail.com>
2024-06-09 19:41:28 +0330

    ok

--- BDC/Forms/FormDuct.xaml.cs
+++ BDC/Forms/FormDuct.xaml.cs
@@ -22,6 +22,7 @@ namespace BDC.Forms
         public FormDuct()
         {
             InitializeComponent();
+
         }
     }
 }
---
+++ BDC/View/BooleanToEnabledConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace BDC.View
+{
+    public class BooleanToEnabledConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isChecked)
+            {
+                return isChecked;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
